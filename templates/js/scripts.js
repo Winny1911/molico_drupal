9243ee8f1627e178ddfdb5bb3cb31c7d91584218
Winny Pina <winny.pina@xpi.com.br>
2021-01-24 17:01:50 -0300

    Projeto Molico feito com CMS Drupal

---
+++ templates/js/scripts.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function() {
+	
+	/*
+	    Carousel
+	*/
+	$('#carousel-example').on('slide.bs.carousel', function (e) {
+
+	    /*
+	        CC 2.0 License Iatek LLC 2018
+	        Attribution required
+	    */
+	    var $e = $(e.relatedTarget);
+	    var idx = $e.index();
+	    var itemsPerSlide = 5;
+	    var totalItems = $('.carousel-item').length;
+	    
+	    if (idx >= totalItems-(itemsPerSlide-1)) {
+	        var it = itemsPerSlide - (totalItems - idx);
+	        for (var i=0; i<it; i++) {
+	            // append slides to end
+	            if (e.direction=="left") {
+	                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+	            }
+	            else {
+	                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+	            }
+	        }
+	    }
+	});
+
+	// Scroll
+
+	// Select all links with hashes
+	$('a[href*="#"]')
+	// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function(event) {
+	// On-page links
+	if (
+		location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+		&& 
+		location.hostname == this.hostname
+	) {
+		// Figure out element to scroll to
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		// Does a scroll target exist?
+		if (target.length) {
+		// Only prevent default if animation is actually gonna happen
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: target.offset().top
+		}, 1000, function() {
+			// Callback after animation
+			// Must change focus!
+			var $target = $(target);
+			$target.focus();
+			if ($target.is(":focus")) { // Checking if the target was focused
+			return false;
+			} else {
+			$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+			$target.focus(); // Set focus again
+			};
+		});
+		}
+	}
+	});
+	
+});
